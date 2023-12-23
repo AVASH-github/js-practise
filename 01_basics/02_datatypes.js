fa6bec051a998d82918d79f68e1b0427ac7abdd6
AVASH-github <148526107+AVASH-github@users.noreply.github.com>
2023-12-23 18:18:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3)//code readability should be high
+
+
+    let name="Avash"
+
+    let age=23
+
+    let isLoggedIn= false
+
+    // number => 2 to power 53
+    // bigint 
+    // string => ""
+    // boolean => true/ false
+    //null => standalone value
+    //undefined => 
+    // symbol => unique 
+
+
+// object 
+
+console.log(typeof null);

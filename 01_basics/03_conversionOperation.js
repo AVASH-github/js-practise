2520e3a56224494c3e92287e50f455c2b08d1dfa
AVASH-github <148526107+AVASH-github@users.noreply.github.com>
2023-12-26 04:53:13 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=33
+// "33abc=> NaN" //not a number
+// true=> 1; false=> 0
+//null=> 0
+
+let isLoggedIn="ads"
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1=> true; 0=> false
+//""=> flase
+//"hitesh"=> true
+
+let someNumber = true
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
